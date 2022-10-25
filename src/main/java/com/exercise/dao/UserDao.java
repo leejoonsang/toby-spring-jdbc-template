cd19dfa8c6c193621b58e8eeee31ebaa619ac24d
Sangjoon <djm04212@naver.com>
2022-10-25 11:04:21 +0900

    toby spring - initial push

---
+++ src/main/java/com/exercise/dao/UserDao.java
@@ -0,0 +1,125 @@
+package com.exercise.dao;
+
+import com.exercise.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.sql.*;
+import java.util.Map;
+
+public class UserDao {
+
+    private ConnectionMaker connectionMaker;
+
+    public UserDao(){
+        this.connectionMaker = new AWSConnectionMaker();
+    }
+
+    public UserDao(ConnectionMaker connectionMaker){
+        this.connectionMaker = connectionMaker;
+    }
+
+    public void add(User user) throws SQLException {
+        Connection conn = connectionMaker.makeConnection();
+        PreparedStatement pstmt = conn.prepareStatement(
+                "INSERT INTO users(id, name, password) values (?, ?, ?)");
+        pstmt.setString(1, user.getId());
+        pstmt.setString(2, user.getName());
+        pstmt.setString(3, user.getPassword());
+
+        pstmt.executeUpdate();
+
+        pstmt.close();
+        conn.close();
+    }
+
+    public User findById(String id) throws SQLException {
+        Connection conn = connectionMaker.makeConnection();
+        PreparedStatement pstmt = conn.prepareStatement(
+                "select * from users where id=?");
+        pstmt.setString(1, id);
+
+        ResultSet rs = pstmt.executeQuery();
+        User user = null;
+        if(rs.next()){
+            user = new User(rs.getString("id"),
+                    rs.getString("name"), rs.getString("password"));
+        }
+
+        rs.close();
+        pstmt.close();
+        conn.close();
+
+        if(user == null){ throw new EmptyResultDataAccessException(1); }
+
+        return user;
+    }
+
+    public void deleteAll() throws SQLException {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+
+            conn = connectionMaker.makeConnection();
+            pstmt = conn.prepareStatement("delete from users");
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(pstmt != null){
+                try{
+                    pstmt.close();
+                }catch (SQLException e){
+                }
+            }
+            if(conn != null){
+                try{
+                    conn.close();
+                }catch (SQLException e){
+                }
+            }
+        }
+    }
+
+    public int getCount() throws SQLException {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = connectionMaker.makeConnection();
+            pstmt = conn.prepareStatement("select count(*) from users");
+            rs = pstmt.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(rs != null){
+                try{
+                    rs.close();
+                }catch (SQLException e){
+                }
+            }
+            if(pstmt != null){
+                try{
+                    pstmt.close();
+                }catch (SQLException e){
+                }
+            }
+            if(conn != null){
+                try{
+                    conn.close();
+                }catch (SQLException e){
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        UserDao userDao = new UserDao();
+        userDao.add(new User("20", "mark20", "twenty"));
+    }
+
+}
---
+++ src/test/java/com/exercise/dao/UserDaoTest.java
@@ -0,0 +1,60 @@
+package com.exercise.dao;
+
+import com.exercise.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.swing.*;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    UserDao userDao = new UserDao();
+    User user1 = new User("20", "mark20", "twenty");
+    User user2 = new User("21", "mark21", "twentyone");
+    User user3 = new User("22", "mark22", "twentytwo");
+
+    @Test
+    @DisplayName("insert and select test passed.")
+    void addAndSelect() throws SQLException, ClassNotFoundException {
+        user1 = new User("1", "sangjoon", "1234");
+
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
+
+        User user = userDao.findById(user1.getId());
+        assertEquals(user1.getName(), user.getName());
+        assertEquals(user1.getPassword(), user.getPassword());
+    }
+
+    @Test
+    @DisplayName("count test passed.")
+    void count() throws SQLException, ClassNotFoundException {
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
+        userDao.add(user2);
+        assertEquals(2, userDao.getCount());
+        userDao.add(user3);
+        assertEquals(3, userDao.getCount());
+    }
+
+    @Test
+    void findById() throws SQLException, ClassNotFoundException{
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+            userDao.findById("1000");
+        });
+    }
+
+}
